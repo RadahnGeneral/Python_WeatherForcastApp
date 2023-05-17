3e3825a6be111d920d56d52d501a957baa000b2f
Gerard <dothanhvinh1@gmail.com>
2023-05-17 18:03:38 +0700

    Adds conditions for days and datatype in backend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -6,3 +7,8 @@ days = st.slider("Forcast Days", min_value=1, max_value=5, help="Select the numb
 
 option = st.selectbox("Select data to view",("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+
+dates=["2022-25-10", "2022-26-10", "2022-27-10"]
+temperature=[25,20,22]
+figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
